189d363e07684026205835f8f927f1aa04f9782c
Orchizakia00 <zakiaislam290@gmail.com>
2023-12-29 18:59:59 +0600

    delete and post operation completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const port = process.env.PORT || 5000;
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sdbndcb.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,11 +24,32 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        
+
+        const blogCollection = client.db('blogDB').collection('blogs');
+
+        // blogs api
+        app.get('/blogs', async (req, res) => {
+            const result = await blogCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/blogs', async (req, res) => {
+            const data = req.body;
+            const result = await blogCollection.insertOne(data);
+            res.send(result);
+        })
+
+        app.delete('/blogs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await blogCollection.deleteOne(query);
+            res.send(result);
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-       
+
     }
 }
 run().catch(console.dir);
